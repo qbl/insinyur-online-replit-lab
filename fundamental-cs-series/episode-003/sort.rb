7f82c16cde881e9755e72be8fe2f678a33ec889e
qbl <farabi.iqbal@gmail.com>
2021-05-31 21:15:12 +0700

    Move current sort implementation to a method named bubble_sort

--- fundamental-cs-series/episode-003/sort.rb
+++ fundamental-cs-series/episode-003/sort.rb
@@ -1,9 +1,13 @@
 def sort(input_array)
+  bubble_sort(input_array)
+end
+
+def bubble_sort(input_array)
   unsorted_until_index = input_array.size-1
   
-  while unsorted_until_index >= 0 do
+  while unsorted_until_index > 0 do
     i = 0
-    
+
     while i < unsorted_until_index do
       if input_array[i] > input_array[i+1]
         input_array[i], input_array[i+1] = input_array[i+1], input_array[i] 
