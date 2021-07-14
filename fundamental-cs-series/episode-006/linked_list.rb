23c97451dfd3875d76f2f6ef2bb6deed176cb2b7
qbl <farabi.iqbal@gmail.com>
2021-07-14 17:38:01 +0700

    Add function to read empty linked list

--- fundamental-cs-series/episode-006/linked_list.rb
+++ fundamental-cs-series/episode-006/linked_list.rb
@@ -1,7 +1,13 @@
+require_relative 'node'
+
 class LinkedList
   attr_reader :head
   
   def initialize
     @head = nil
+  end
+
+  def read(index)
+    nil
   end
 end
--- fundamental-cs-series/episode-006/linked_list_spec.rb
+++ fundamental-cs-series/episode-006/linked_list_spec.rb
@@ -8,4 +8,11 @@ describe 'LinkedList' do
       expect(linked_list.head).to eq(nil)
     end
   end
+
+  describe '#read' do
+    it 'should return nil when reading head of newly initialized linked list' do
+      linked_list = LinkedList.new
+      expect(linked_list.read(0)).to eq(nil)
+    end
+  end
 end
