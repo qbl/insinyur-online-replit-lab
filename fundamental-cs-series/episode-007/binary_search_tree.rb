64f5ec8613b308f5e2f987d487b28ae2b9f8bea1
qbl <farabi.iqbal@gmail.com>
2021-07-28 16:13:22 +0700

    Remove current implementation for BinarySearchTree#delete

--- fundamental-cs-series/episode-007/binary_search_tree.rb
+++ fundamental-cs-series/episode-007/binary_search_tree.rb
@@ -28,40 +28,4 @@ class BinarySearchTree
       true
     end
   end
-
-  def delete(value, node=@root, parent_node=nil)
-    if node == nil
-      deleted_node = nil
-    elsif value == node.value
-      deleted_node = node
-
-      if parent_node == nil
-        @root = nil
-      else
-        if node.right
-          parent_node.left = min_value(node.right)
-          delete(parent_node.left.value, node.right, node.right)
-
-          parent_node.left.left = node.left
-          parent_node.left.right = node.right
-        elsif node.left
-          parent_node.left = node.left
-        else
-          parent_node.left = nil
-        end
-      end
-    else
-      deleted_node = delete(value, node.left, node)
-    end
-
-    deleted_node
-  end
-
-  def min_value(node)
-    if node.left == nil
-      node
-    else
-      min_value(node.left)
-    end
-  end
 end
